6e84afd47eaf35bbc315f03aa0118af7e2112eec
AgheraRaj <rajaghera2004@gmail.com>
2025-09-27 10:36:12 +0530

    init CreatorDesk

---
+++ Client/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline flex justify-center items-center h-screen">
+      Hello World
+    </div>
+  )
+}
+
+export default App
